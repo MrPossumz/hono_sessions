3dcb94d90a08e763addb32484654d9d5b20db6c6
Burt <bsickler@spudlabs.com>
2024-04-05 21:03:47 -0700

    bumps hono and iron-webcrypto

--- deps.ts
+++ deps.ts
@@ -1,5 +1,12 @@
-export { nanoid } from 'https://esm.sh/nanoid@4.0.0/async'
-export type { MiddlewareHandler, Context } from 'https://deno.land/x/hono@v3.12.8/mod.ts'
-export { getCookie, setCookie } from 'https://deno.land/x/hono@v3.12.8/helper/cookie/index.ts'
-export type { CookieOptions } from 'https://deno.land/x/hono@v3.12.8/utils/cookie.ts'
-export * as Iron from 'https://esm.sh/iron-webcrypto@0.10.1'
+export { nanoid } from "https://esm.sh/nanoid@4.0.0/async";
+export type {
+  Context,
+  MiddlewareHandler,
+} from "https://deno.land/x/hono@v4.1.3/mod.ts";
+export {
+  getCookie,
+  setCookie,
+} from "https://deno.land/x/hono@v4.1.3/helper/cookie/index.ts";
+export { createMiddleware } from "https://deno.land/x/hono@v4.1.3/helper.ts";
+export type { CookieOptions } from "https://deno.land/x/hono@v4.1.3/utils/cookie.ts";
+export * as Iron from "https://esm.sh/iron-webcrypto@1.1.0";
--- src/Middleware.ts
+++ src/Middleware.ts
@@ -1,119 +1,139 @@
-import { nanoid } from '../deps.ts'
-import { MiddlewareHandler, getCookie, setCookie } from '../deps.ts'
-import Store from './store/Store.ts'
-import CookieStore from './store/CookieStore.ts'
-import { Session, SessionData, encrypt, decrypt } from '../mod.ts'
-import { CookieOptions } from '../deps.ts';
+import { nanoid } from "../deps.ts";
+import { createMiddleware, getCookie, setCookie } from "../deps.ts";
+import Store from "./store/Store.ts";
+import CookieStore from "./store/CookieStore.ts";
+import { decrypt, encrypt, Session, SessionData } from "../mod.ts";
+import { CookieOptions } from "../deps.ts";
 
 interface SessionOptions {
-  store: Store | CookieStore
-  encryptionKey?: string,
-  expireAfterSeconds?: number,
-  cookieOptions?: CookieOptions,
-  sessionCookieName?: string
+  store: Store | CookieStore;
+  encryptionKey?: string;
+  expireAfterSeconds?: number;
+  cookieOptions?: CookieOptions;
+  sessionCookieName?: string;
 }
 
 export function sessionMiddleware(options: SessionOptions) {
-
-  const store = options.store
-  const encryptionKey = options.encryptionKey
-  const expireAfterSeconds = options.expireAfterSeconds
-  const cookieOptions = options.cookieOptions
-  const sessionCookieName = options.sessionCookieName || 'session'
+  const store = options.store;
+  const encryptionKey = options.encryptionKey;
+  const expireAfterSeconds = options.expireAfterSeconds;
+  const cookieOptions = options.cookieOptions;
+  const sessionCookieName = options.sessionCookieName || "session";
 
   if (store instanceof CookieStore) {
-    store.sessionCookieName = sessionCookieName
-  
+    store.sessionCookieName = sessionCookieName;
+
     if (encryptionKey) {
-      store.encryptionKey = encryptionKey
+      store.encryptionKey = encryptionKey;
     } else {
-      throw new Error('encryptionKey is required while using CookieStore. encryptionKey must be at least 32 characters long.')
+      throw new Error(
+        "encryptionKey is required while using CookieStore. encryptionKey must be at least 32 characters long.",
+      );
     }
-  
+
     if (cookieOptions) {
-      store.cookieOptions = cookieOptions
+      store.cookieOptions = cookieOptions;
     }
   }
 
-  const middleware: MiddlewareHandler = async (c, next) => {
-    const session = new Session
-    let sid = ''
-    let session_data: SessionData | null | undefined
-    let createNewSession = false
+  const middleware = createMiddleware(async (c, next) => {
+    const session = new Session();
+    let sid = "";
+    let session_data: SessionData | null | undefined;
+    let createNewSession = false;
 
-    const sessionCookie = getCookie(c, sessionCookieName)
-  
+    const sessionCookie = getCookie(c, sessionCookieName);
+
     if (sessionCookie) { // If there is a session cookie present...
-
       if (store instanceof CookieStore) {
-        session_data = await store.getSession(c)
+        session_data = await store.getSession(c);
       } else {
         try {
-          sid = (encryptionKey ? await decrypt(encryptionKey, sessionCookie) : sessionCookie) as string
-          session_data = await store.getSessionById(sid)
+          sid = (encryptionKey
+            ? await decrypt(encryptionKey, sessionCookie)
+            : sessionCookie) as string;
+          session_data = await store.getSessionById(sid);
         } catch {
-          createNewSession = true
+          createNewSession = true;
         }
       }
 
       if (session_data) {
-        session.setCache(session_data)
+        session.setCache(session_data);
 
         if (session.sessionValid()) {
-          session.reupSession(expireAfterSeconds)
+          session.reupSession(expireAfterSeconds);
         } else {
-          store instanceof CookieStore ? await store.deleteSession(c) : await store.deleteSession(sid)
-          createNewSession = true
+          store instanceof CookieStore
+            ? await store.deleteSession(c)
+            : await store.deleteSession(sid);
+          createNewSession = true;
         }
       } else {
-        createNewSession = true
+        createNewSession = true;
       }
     } else {
-      createNewSession = true
+      createNewSession = true;
     }
 
     if (createNewSession) {
       const defaultData = {
-        _data:{},
+        _data: {},
         _expire: null,
         _delete: false,
         _accessed: null,
-      }
+      };
 
       if (store instanceof CookieStore) {
-        await store.createSession(c, defaultData)
+        await store.createSession(c, defaultData);
       } else {
-        sid = await nanoid(21)
-        await store.createSession(sid, defaultData)
+        sid = await nanoid(21);
+        await store.createSession(sid, defaultData);
       }
 
-      session.setCache(defaultData)
+      session.setCache(defaultData);
     }
-  
+
     if (!(store instanceof CookieStore)) {
-      setCookie(c, sessionCookieName, encryptionKey ? await encrypt(encryptionKey, sid) : sid, cookieOptions)
+      setCookie(
+        c,
+        sessionCookieName,
+        encryptionKey ? await encrypt(encryptionKey, sid) : sid,
+        cookieOptions,
+      );
     }
 
-    session.updateAccess()
+    session.updateAccess();
 
-    c.set('session', session)
+    c.set("session", session);
 
-    await next()
+    await next();
 
-    if (c.get('session_key_rotation') === true && !(store instanceof CookieStore)) {
-      await store.deleteSession(sid)
-      sid = await nanoid(21)
-      await store.createSession(sid, session.getCache())
+    if (
+      c.get("session_key_rotation") === true && !(store instanceof CookieStore)
+    ) {
+      await store.deleteSession(sid);
+      sid = await nanoid(21);
+      await store.createSession(sid, session.getCache());
 
-      setCookie(c, sessionCookieName, encryptionKey ? await encrypt(encryptionKey, sid) : sid, cookieOptions)
+      setCookie(
+        c,
+        sessionCookieName,
+        encryptionKey ? await encrypt(encryptionKey, sid) : sid,
+        cookieOptions,
+      );
     }
 
-    store instanceof CookieStore ? await store.persistSessionData(c, session.getCache()) : await store.persistSessionData(sid, session.getCache())
+    store instanceof CookieStore
+      ? await store.persistSessionData(c, session.getCache())
+      : await store.persistSessionData(sid, session.getCache());
 
     if (session.getCache()._delete) {
-      store instanceof CookieStore ? await store.deleteSession(c) : await store.deleteSession(sid)
+      store instanceof CookieStore
+        ? await store.deleteSession(c)
+        : await store.deleteSession(sid);
     }
-  }
+  });
 
-  return middleware
+  return middleware;
 }
